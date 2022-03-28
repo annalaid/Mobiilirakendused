fd099a34607a1808865947e29611e4a629dd11fa
Anna-Stiina Laidna <70900256+annalaid@users.noreply.github.com>
2022-03-28 11:31:03 +0300

    start

--- mobrakendused/src/components/Calendar.tsx
+++ mobrakendused/src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-/*import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Calender() {
     return (
@@ -15,4 +15,4 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#1aa'
     }
-});*/
+});
