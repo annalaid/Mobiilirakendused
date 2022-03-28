0de3a96347b56caf8ff83e4e176e39b085c9cd13
Anna-Stiina Laidna <70900256+annalaid@users.noreply.github.com>
2022-03-28 11:25:31 +0300

    start

---
+++ mobrakendused/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StyleHF.css';
+
+function Footer() {
+    return (
+        <footer>
+            <p>this is a footer</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ mobrakendused/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './StyleHF.css';
+
+
+function Header() {
+    return (
+        <p>this is a header</p>
+    );
+}
+export default Header;
