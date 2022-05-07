963f40a153da85503f65c20e8a23b1c2efdb885f
Karl Aleksander Leoste <KarlAleksander.Leoste@tptlive.ee>
2022-05-07 18:12:26 +0300

    Add previous, current, next week buttons with functionality

--- mobrakendused/src/components/Calendar.tsx
+++ mobrakendused/src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, StyleSheet, Text, View, ScrollView } from 'react-native';
 import './StyleHF.css';
 import Entry from '../types/Entry';
@@ -32,71 +32,117 @@ const entryList: Entry[] = [
     }
 ];
 
-/*
-    construct a displayable list out of entryList
-*/
-
-let dayList: Day[] = [];
-
-for (let i = 0; i < 8; i++) {
-    const date = new Date();
-    date.setDate(date.getDate() + i);
-
-    dayList.push({
-        entryList: [],
-        time: date
-    })
-}
-
-entryList.forEach(entry => {
-    dayList.forEach(day => {
-        if (day.time.getDate() == entry.time.getDate()) {
-            day.entryList.push(entry)
-        }
-    });
-});
-
-/*
-console.log(entryList);
-console.log(dayList);
-*/
-
 export default function Calender() {
+    const [currentDate, setCurrentDate] = useState(new Date());
+
+    const [dayList, setDayList] = useState<Day[]>([]);
+
+    /*
+    construct a displayable list out of entryList
+    */
+
+    useEffect(() => {
+        let dayList: Day[] = [];
+
+        // 1/2 pre-populating is necessary to only show tasks of the current week
+        for (let i = 1; i < 8; i++) {
+            const date = new Date(currentDate);
+            date.setDate(date.getDate() - date.getDay() + i);
+
+            dayList.push({
+                entryList: [],
+                time: date
+            })
+        }
+
+        // 2/2 only tasks that match with pre-populated days are used
+        entryList.forEach(entry => {
+            dayList.forEach(day => {
+                let d = day.time;
+                let e = entry.time;
+                if (d.getFullYear() === e.getFullYear() &&
+                    d.getMonth() === e.getMonth() &&
+                    d.getDate() === e.getDate()) {
+                    day.entryList.push(entry)
+                }
+            });
+        });
+
+        setDayList(dayList);
+    }, [currentDate]);
+
+
+
+    let addDaysToCurrentDate = (days: number) => {
+        let date = new Date(currentDate);
+        date.setDate(date.getDate() + days);
+        setCurrentDate(date);
+    }
+
+    let onPressPreviousWeek = () => {
+        addDaysToCurrentDate(-7);
+    }
+
+    let onPressNextWeek = () => {
+        addDaysToCurrentDate(7);
+    }
+
+    let onPressCurrentWeek = () => {
+        setCurrentDate(new Date());
+    }
+
     return (
         <View style={styles.overhead}>
             <View style={styles.titleList}>
-                <View style={styles.titleText}>
+                <View style={styles.titleTitle}>
                     <Text style={styles.text}>
-                        {new Date().toLocaleDateString("et-EE", { month: "long" })}
+                        {new Date().toLocaleDateString("et-EE", { weekday: "long" })}
                     </Text>
                 </View>
                 <View style={styles.titleButton}>
-                    <Button title={"new entry"} >
+                    <Button title={"uus kirje"} >
 
                     </Button>
                 </View>
             </View>
-            
+            <View style={styles.titleList}>
+                <View style={styles.titleButton}>
+                    <Button title={"eelmine"} onPress={onPressPreviousWeek} >
+
+                    </Button>
+                </View>
+                <View style={styles.titleText}>
+                    <Text style={styles.text} onPress={onPressCurrentWeek}>
+                        {currentDate.toLocaleDateString("et-EE", {})}
+                    </Text>
+                </View>
+                <View style={styles.titleButton}>
+                    <Button title={"järgmine"} onPress={onPressNextWeek} >
+
+                    </Button>
+                </View>
+            </View>
+
             <ScrollView style={styles.dayList}>
                 {dayList.map(day =>
-                <View style={styles.day}>
-                    <View style={styles.dayEntryList}>
-                        {day.entryList.map(entry => 
-                        <View style={styles.entry}>
-                            <Text style={styles.text}>{entry.title}</Text>
+                    <View style={styles.day}>
+                        <View style={styles.dayEntryList}>
+                            {day.entryList.map(entry =>
+                                <View style={styles.entry}>
+                                    <Text style={styles.text}>{entry.title}</Text>
+                                </View>
+                            )}
                         </View>
-                        )}
-                    </View>
 
-                    <View style={styles.dayInfo}>
-                        <Text style={styles.text}>
-                            {day.time.toLocaleDateString("et-EE", { weekday: "long" })}
-                        </Text>
-                        <Text style={styles.text}>
-                            {day.time.toLocaleDateString("et-EE", { })}
-                        </Text>
+                        <View style={styles.dayInfo}>
+                            <Text style={styles.text}>
+                                {day.time.toLocaleDateString("et-EE", { weekday: "long" })}
+                            </Text>
+                            <Text style={styles.text}>
+                                {day.time.toLocaleDateString("et-EE", {})}
+                            </Text>
+                        </View>
                     </View>
-                </View>
                 )}
             </ScrollView>
         </View>
@@ -115,12 +161,13 @@ const styles = StyleSheet.create({
     },
     dayList: {
         flexDirection: "column",
-        paddingBottom: "0.25em",        
-        backgroundColor: colorText,        
+        paddingBottom: "0.25em",
+        backgroundColor: colorText,
+        marginTop: "1em"
     },
-    day: {        
+    day: {
         flexDirection: "row",
-        marginTop: "0.25em",        
+        marginTop: "0.25em",
     },
     dayEntryList: {
         flex: 8,
@@ -144,13 +191,19 @@ const styles = StyleSheet.create({
     },
     titleList: {
         flexDirection: "row",
-        paddingVertical: "1em",
+        paddingTop: "1em",
+    },
+    titleTitle: {
+        flex: 8,
+        flexDirection: "row",
     },
     titleText: {
-        flex: 8,
+        flex: 4,
+        justifyContent: "center",
+        textAlign: "center"
     },
     titleButton: {
-        flex: 4,    
-        marginVertical: "0.25em",        
+        flex: 4,
+        marginVertical: "0.25em",
     }
 });
